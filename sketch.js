3b166781b621d2dfae18b1d183245aa85a284d15
Laffyquarkz <71347293+Laffyquarkz@users.noreply.github.com>
2020-10-27 13:10:14 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var bullet,wall,thickness
+var speed,weight;
+
+function setup() {
+  createCanvas(1600,400);
+ thickness=random(22,83)
+ 
+  weight=random(30,52)
+  speed=random(223,321)
+  bullet=createSprite(50,200,50,20)
+  bullet.shapeColor=("white")
+  wall=createSprite(1200,200,thickness,height/2)
+  bullet.velocityX=speed;
+}
+
+function draw() {
+  background("black");  
+  if(hasCollided (bullet,wall))
+  {
+    bullet.velocityX=0;
+    var deformation=0.5*weight*speed*speed/(thickness*thickness*thickness);
+    if(deformation>10)
+    {
+        wall.shapeColor=color(255,0,0)
+    }
+ 
+    if(deformation<10)
+    {
+        wall.shapeColor=color(0,255,0)
+    }
+
+    
+  }
+
+  drawSprites();
+}
+function hasCollided(lbullet,lwall){
+bulletRightEdge=lbullet.x+lbullet.width;
+wallLeftEdge=lwall.x;
+if (bulletRightEdge>=wallLeftEdge)
+{
+  return true
+}
+return false
+}
+
+
+
+
+
